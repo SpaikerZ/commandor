74b934baa15d4b53e80c2ab4f68e796afb3a5614
Egor Gordiyevsky <65719990+SpaikerZ@users.noreply.github.com>
2020-12-13 10:52:26 +0300

    __deleter__

--- commandor/creater.py
+++ commandor/creater.py
@@ -1,6 +1,29 @@
+import os
 
 def create_file():
-    print("I was created file")
+    print()
+    print('Write file name')
+    file_name = str(input())
+    print('Write file extension (for example .txt or .docx)')
+    extension_file = str(input())
+    print('write path of file \n if you print nothing you will create file in the commandor folder')
+    path_file = str(input())
+    if (path_file == False):
+        path_file = './'
+
+    file = open(path_file + file_name + extension_file, 'w')
+
+
+
 
 def create_folder():
-    print('I was created folder')
+    print()
+    print('Write folder name')
+    folder_name = str(input())
+    print('\n')
+    print('Write folder path \n if you print nothing you will create folder in commandor')
+    folder_path = str(input())
+    if folder_path == False:
+        folder_path = './'
+
+    os.mkdir(folder_path + folder_name)
--- commandor/deleter.py
+++ commandor/deleter.py
@@ -1,5 +1,25 @@
+import os
 def delete_file():
-    print("I was deleted something")
+    print()
+    print('Write file name')
+    file_name = str(input())
+    print('Write file extension (for example .txt or .docx)')
+    extension_file = str(input())
+    print('write path of file \n if you print nothing you will delete file in the commandor folder')
+    path_file = str(input())
+    if (path_file == False):
+        path_file = './'
+
+    os.remove(path_file + file_name + extension_file)
 
 def delete_folder():
-    print('I was deleted folder')
+    print()
+    print('Write folder name')
+    folder_name = str(input())
+    print('\n')
+    print('Write folder path \n if you print nothing you will delete folder in commandor')
+    folder_path = str(input())
+    if folder_path == False:
+        folder_path = './'
+
+    os.rmdir(folder_path + folder_name)
